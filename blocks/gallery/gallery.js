d8586449b3ef0333179463d4792523ecbb6e8f14
frankbiederich <fbiederi@adobe.com>
2022-12-21 12:37:15 +0100

    trying to tweak wrapper

--- blocks/gallery/gallery.js
+++ blocks/gallery/gallery.js
@@ -14,7 +14,7 @@ window.addEventListener('DOMContentLoaded', () => {
 
   document.querySelectorAll('.gallery').forEach(($gallery) => {
     // wrap thumbnails with links to the full version
-    const $gallerySection = $gallery.closest('.section-wrapper');
+    const $gallerySection = $gallery.closest('.gallery-container');
     $gallery.querySelectorAll('picture').forEach(($pic) => {
       const $img = $pic.querySelector('img');
       const fullUrl = $img.src
@@ -25,7 +25,7 @@ window.addEventListener('DOMContentLoaded', () => {
       $pic.remove();
     });
     
-    const sections = Array.from(document.querySelectorAll('.section-wrapper'));
+    const sections = Array.from(document.querySelectorAll('.gallery-container'));
     const hasHeader = sections[0] && sections[0].classList.contains('hero');
     if (hasHeader) {
       // add gallery below title if header section
